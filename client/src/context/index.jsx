40c556624520d5d8149189a194e7d8b5d99fd732
Imam Abubakar <imamabubakar2002@gmail.com>
2022-12-15 14:53:08 +0000

    contract deployment

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0xb90269A1870E73dD219f0A06544F4b722ce06f51');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
